c5e2f8e00756d7fb9b3efef9653f93f6f888dbfd
mcostarodrigo <m.costa.rodrigo.rc@gmail.com>
2023-01-31 21:58:43 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import { Container, H1, P } from './styles';
+
+const App = () => {
+
+  const hello = "Hello React";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Container>
+    <H1 >{hello}</H1>
+    <P>Esse é meu parágrafo </P>
+    <P>Esse é meu outro parágrafo</P>
+  </Container>
   );
+
 }
 
-export default App;
+export default App
---
+++ src/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+background-color: red;
+`;
+
+export const H1 = styled.h1`
+color: blue;
+font-size: 20px;
+
+`;
+
+export const P = styled.p `
+color: purple;
+font-size: 14px;
+`
