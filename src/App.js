4024bfb435b8e9bd4f797434e726a4c51ff073a7
mcostarodrigo <m.costa.rodrigo.rc@gmail.com>
2023-02-06 22:47:45 -0300

    atualizando

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import People from "./assets/People.png"
+import Arrow from "./assets/arrow.png"
 
 
 import {
@@ -15,7 +17,7 @@ const App = () => {
 
   return (
     <Container>
-      <Image />
+      <Image alt="logo-imagem" src={People}/>
       <ContainerItens>
         <H1>Olá</H1>
 
@@ -25,7 +27,7 @@ const App = () => {
         <InputLabel>Idade</InputLabel>
         <Input placeholder="Idade"/>
 
-        <Button>Cadastrar</Button>
+        <Button>Cadastrar <img alt="seta" src={Arrow}/> </Button>
 
         
       </ContainerItens>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import globalStyle from './globalStyles';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <>
+        <App /> <globalStyle />
+    </>,
+    document.getElementById('root')
+);
 
